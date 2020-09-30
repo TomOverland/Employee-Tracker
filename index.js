15a23bda5d3a7768fde005cf0365b8130e91bffc
Tom Overland <tomoverland03@gmail.com>
2020-09-30 09:15:02 -0500

    Created initial inquirer prompts

--- index.js
+++ index.js
@@ -15,3 +15,43 @@ connection.connect(function(err) {
     console.log("connected as ID " + connection.threadId);
     connection.end();
 });
+
+function init(){
+    inquirer.prompt({
+        name: "menu",
+        type: "list",
+        message: "Welcome to the Employee Tracker",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "View All Employees By Role",
+            "Add Employee",
+            "Remove Empoyee",
+            "Add Role",
+            "Delete Role",
+            "Add Department",
+            "Delete Department",
+            "Update Employee Role",
+            "Update Employee Manager"
+        ]
+    }).then((answer) => {
+        switch (answer.menu) {
+            case "View All Employees":
+                viewAllEmployees();
+                break;
+
+            case "View All Employees By Department":
+                viewAllEmployeesByDepartment();
+                break;
+        }
+    })
+}
+
+function viewAllEmployees(){
+    console.log("This is where you'd be able to view all employees");
+};
+
+function viewAllEmployeesByDepartment(){
+    console.log("This is where you'd be able to view all employees by department");
+}
