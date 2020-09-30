b86fbf6721bb26813ddfbad5704c55475acecbaa
Tom Overland <tomoverland03@gmail.com>
2020-09-30 08:43:29 -0500

    created schema sql file

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+//setting up mySQL
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: ""
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as ID " + connection.threadId);
+    connection.end();
+});
